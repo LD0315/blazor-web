634341d132240eaa107ecc394e76624b75098fba
Linlin Deng <linlin.deng@icemedia.com.au>
2022-12-12 16:40:08 +1000

    ran command add migration

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using blazor_web.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazor_web.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+  
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+    
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,7 +1,9 @@
+using blazor_web.Server.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,10 +20,16 @@ namespace blazor_web.Server
 
         public IConfiguration Configuration { get; }
 
+      
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSwaggerGen();
