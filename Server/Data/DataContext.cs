5dfff0043afb79b17c689d6f0bd05a8254f933ff
Linlin Deng <linlin.deng@icemedia.com.au>
2022-12-14 17:11:01 +1000

    update

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -5,14 +5,14 @@ namespace blazor_web.Server.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext()
-        {
-  
-        }
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        
+        public DataContext() : base()
         {
 
         }
+        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { Database.EnsureCreated(); }
+       
         
         public DbSet<Product> Products { get; set; }
     }
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -20,16 +20,17 @@ namespace blazor_web.Server
 
         public IConfiguration Configuration { get; }
 
-      
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+           
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSwaggerGen();
